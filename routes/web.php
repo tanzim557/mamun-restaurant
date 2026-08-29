3f7481af041d032edf743b3b75b841e494093fd5
MUBASSHER ALAM TANZIM <mubassheralam0@gmail.com>
2026-08-29 23:34:54 +0600

    Completely remove Gallery page and navigation links

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 Route::view('/menu', 'menu')->name('menu');
 Route::view('/order', 'order')->name('order');
-Route::view('/gallery', 'gallery')->name('gallery');
+Route::redirect('/gallery', '/menu');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/admin', 'admin.login')->name('admin.login');
